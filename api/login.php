ca4de235be3ac26a769d1fa21555af88d8225e8e
huei2025ava <avachen031@gmail.com>
2026-01-04 23:53:35 +0800

    登入成功，彈出視窗，顯示歡迎管理者id

--- api/login.php
+++ api/login.php
@@ -1,8 +1,16 @@
 <?php
 include_once "db.php";
+
 $chk = $Admin->count($_POST);
+
+// $user = $Admin->find($_POST);
 if ($chk) {
   $_SESSION['admin']=$_POST['acc'];
+  // $_SESSION['admin_id']=$user['id'];
+  // echo "<script>alert('歡迎回來，管理員 id：{$_SESSION['admin_id']}');
+  // location.href = '../back.php';
+  // </script>
+  // ";
   to("../back.php");
 }
 ?>
