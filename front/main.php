2cc17b1758778742f74784c3bf419ea638a2f832
huei20251002 <avachen031@gmail.com>
2026-03-02 13:59:02 +0800

    完成動態文字廣告跑馬燈顯示

--- front/main.php
+++ front/main.php
@@ -1,6 +1,13 @@
  <div class="di"
      style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
      <marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
+         <?php
+          $ads = $Ad->all(['sh' => 1]);
+          foreach ($ads as $ad) {
+              echo $ad['text']."&nbsp;&nbsp;&nbsp;&nbsp;";
+
+          }
+         ?>
      </marquee>
      <div style="height:32px; display:block;"></div>
      <!--正中央-->
@@ -14,10 +21,10 @@
 
      <?php
        $mvims = $Mvim->all(['sh' => 1]);
-       foreach ($mvims as $mv) {
-           echo "lin.push('upload/{$mv['img']}');\n";
-       }
-     ?>
+         foreach ($mvims as $mv) {
+             echo "lin.push('upload/{$mv['img']}');\n";
+         }
+         ?>
 
      var now = 0;
      if (lin.length > 1) {
